19b2bedc55922b7535750c5d12f949ab5c696fc0
NEXRYDE <nexryde@admoblordgroup.com>
2026-01-30 10:37:12 +0000

    auto-commit for f6ee4f38-e936-4799-b06e-2cdc67739a85

--- debug_test.py
+++ debug_test.py
@@ -2,7 +2,7 @@
 import requests
 
 def test_user_blocking():
-    base_url = "https://rideapp-admin-1.preview.emergentagent.com/api"
+    base_url = "https://ride-location-fix.preview.emergentagent.com/api"
     test_user_id = "test-user-123"
     
     # Test unblock user
