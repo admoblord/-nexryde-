4479b22867b82b08503e1ee514f70f9a7bd8a973
NEXRYDE <nexryde@admoblordgroup.com>
2026-01-30 09:44:49 +0000

    auto-commit for b286e63e-b07b-4fb9-abce-c7e7df806e1c

--- websocket_features_test.py
+++ websocket_features_test.py
@@ -13,7 +13,7 @@ import uuid
 import sys
 
 # Backend URL from frontend .env
-BACKEND_URL = "https://nexryde-test.preview.emergentagent.com/api"
+BACKEND_URL = "https://docs-hub-7.preview.emergentagent.com/api"
 WS_URL = "wss://nexryde-admin.preview.emergentagent.com/ws"
 
 class WebSocketFeaturesTester:
