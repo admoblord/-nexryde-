9f94326a5cb6945013eb265b05b1bf3331eed195
NEXRYDE <nexryde@admoblordgroup.com>
2026-02-02 14:13:05 +0000

    auto-commit for 08112268-853f-4f2b-adc0-599cf47f771f

--- frontend/app/(rider-tabs)/rider-home.tsx
+++ frontend/app/(rider-tabs)/rider-home.tsx
@@ -93,7 +93,7 @@ export default function RiderHomeScreen() {
               <Image
                 source={{ uri: RIDER_HERO }}
                 style={{ width: '100%', height: 200, borderTopLeftRadius: 28, borderTopRightRadius: 28 }}
-                resizeMode="cover"
+                contentFit="cover"
               />
               
               {/* Content Section Below Image */}
