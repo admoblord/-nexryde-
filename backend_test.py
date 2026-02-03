1f680cd314177fa54441f2c8a26b3bb1d86c5599
NEXRYDE <nexryde@admoblordgroup.com>
2026-02-03 10:22:30 +0000

    auto-commit for ba98ced2-2c47-4f5b-a142-8200d9790e1b

--- backend_test.py
+++ backend_test.py
@@ -588,9 +588,9 @@ class NexrydeAPITester:
     
     async def test_wallet_topup(self):
         """Test POST /api/wallet/{user_id}/topup"""
-        response = await self.make_request("POST", f"/wallet/{TEST_USER_ID}/topup?amount=5000", {
-            "payment_method": "bank_transfer",
-            "reference": "TEST_TOPUP_123"
+        response = await self.make_request("POST", f"/wallet/user_123/topup", {
+            "amount": 1000,
+            "payment_method": "card"
         })
         
         if response["success"]:
