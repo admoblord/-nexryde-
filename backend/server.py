2a5e0f678a9b607b068d8159b17bf81185103098
NEXRYDE <nexryde@admoblordgroup.com>
2026-02-06 15:35:36 +0000

    auto-commit for 4e187513-b9e4-4a14-80aa-dcf217ae13f4

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, APIRouter, HTTPException, status, Response, Request, WebSocket, WebSocketDisconnect, Form, File, UploadFile
+from fastapi import FastAPI, APIRouter, HTTPException, status, Response, Request, WebSocket, WebSocketDisconnect, Form, File, UploadFile, Body
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 from dotenv import load_dotenv
