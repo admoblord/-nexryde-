3417a9880c32678250ffd0ffb423990866d34283
NEXRYDE <nexryde@admoblordgroup.com>
2026-02-06 14:57:44 +0000

    auto-commit for f3d500e5-f07b-48a2-98b9-8f6929f00d08

--- frontend/app/(rider-tabs)/rider-home.tsx
+++ frontend/app/(rider-tabs)/rider-home.tsx
@@ -14,6 +14,7 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import { useRouter } from 'expo-router';
 import { Ionicons } from '@expo/vector-icons';
 import { LinearGradient } from 'expo-linear-gradient';
+import ActiveTripBar from '@/src/components/ActiveTripBar';
 
 const { width } = Dimensions.get('window');
 
