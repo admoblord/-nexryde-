5a082aede5f9d1d1ac0f03e96d4feee87c27f557
NEXRYDE <nexryde@admoblordgroup.com>
2026-02-06 11:17:55 +0000

    auto-commit for da4b068a-f15d-4bc4-9c51-5eb9bd6b749e

--- frontend/app/(driver-tabs)/driver-home.tsx
+++ frontend/app/(driver-tabs)/driver-home.tsx
@@ -8,6 +8,7 @@ import {
   Dimensions,
   Animated,
   StatusBar,
+  ActivityIndicator,
 } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useRouter } from 'expo-router';
