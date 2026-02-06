3f9a3444ceff6d850c3fb77594781d02954f67e0
NEXRYDE <nexryde@admoblordgroup.com>
2026-02-06 08:20:27 +0000

    auto-commit for 267aede4-1170-4a6e-b1d9-37ed60f207f5

---
+++ frontend/src/components/RideMap.web.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// Web version - empty placeholder (map only shows on native)
+export default function RideMap() {
+  return <View style={{ height: 0 }} />;
+}
